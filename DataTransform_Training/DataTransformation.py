4af6e026eb1885e2745415ea5c2f5d62b6feae9b
RahieGadekar10 <68743282+RahieGadekar10@users.noreply.github.com>
2022-04-12 18:46:11 +0530

    Add files via upload

---
+++ DataTransform_Training/DataTransformation.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+from os import listdir
+from application_logging.logger import App_Logger
+import pandas as pd
+
+
+class dataTransform:
+
+     def __init__(self):
+          self.goodDataPath = "Training_Raw_files_validated/Good_Raw"
+          self.logger = App_Logger()
+
+
+     def addQuotesToStringValuesInColumn(self):
+
+          log_file = open("Training_Logs/addQuotesToStringValuesInColumn.txt", 'a+')
+          try:
+               onlyfiles = [f for f in listdir(self.goodDataPath)]
+               for file in onlyfiles:
+                    data = pd.read_csv(self.goodDataPath+"/" + file)
+                    data['DATE'] = data["DATE"].apply(lambda x: "'" + str(x) + "'")
+
+                    data.to_csv(self.goodDataPath+ "/" + file, index=None, header=True)
+                    self.logger.log(log_file," %s: Quotes added successfully!!" % file)
+          except Exception as e:
+               self.logger.log(log_file, "Data Transformation failed because:: %s" % e)
+               log_file.close()
+          log_file.close()
